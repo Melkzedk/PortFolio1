84477a7835d1be97ddd5f4e222777c0ec02307c0
Melkzedk <79189172+Melkzedk@users.noreply.github.com>
2021-08-03 10:07:35 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const navbar = document.querySelector(".navbar");
+
+// add shadow to navbar
+window.onscroll = () => {
+  if (window.pageYOffset > 30) {
+    navbar.classList.add("navbar-shadow");
+  } else {
+    navbar.classList.remove("navbar-shadow");
+  }
+};
+
+// dynamic footer year
+year.innerHTML = new Date().getFullYear();
